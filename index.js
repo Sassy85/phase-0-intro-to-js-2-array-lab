cd8fc149076d432b92f33940a7330204c3289f91
hayleerees <rae8504679@gmail.com>
2023-09-01 19:36:59 -0400

    Another One Bites the Dust

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}
+function appendCat(name) {
+ const newCats = [...cats, name]
+ return newCats   
+}
+function prependCat(name) {
+    const newNewCats = [name, ...cats]
+    return newNewCats
+}
+function removeLastCat(name) {
+    const newNewNewCats = [...cats]
+    newNewNewCats.pop()
+    return newNewNewCats
+}
+function removeFirstCat(name) {
+    const newNewNewNewCats = [...cats]
+    newNewNewNewCats.shift()
+    return newNewNewNewCats
+}
